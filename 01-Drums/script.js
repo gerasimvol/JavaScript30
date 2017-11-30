b10c92bf98dfeb7f8366c3678b9cddd3d101bdde
gerasimvol <volodik1802@gmail.com>
2017-11-30 23:42:28 +0200

    Replace audio in JS

---
+++ 01-Drums/script.js
@@ -0,0 +1,45 @@
+  window.addEventListener("keydown", keyDown, false)
+  window.addEventListener("keyup", keyUp, false)
+
+  
+  let boom = new Audio("sounds/boom.wav"),
+      clap = new Audio("sounds/clap.wav"),
+      hihat = new Audio("sounds/hihat.wav"),
+      kick = new Audio("sounds/kick.wav"),
+      openhat = new Audio("sounds/openhat.wav"),
+      ride = new Audio("sounds/ride.wav"),
+      snare = new Audio("sounds/snare.wav"),
+      tink = new Audio("sounds/tink.wav"),
+      tom = new Audio("sounds/tom.wav")
+  
+  const sounds = {
+    65: boom,
+    83: clap,
+    68: hihat,
+    70: kick,
+    71: openhat,
+    72: ride,
+    74: snare,
+    75: tink,
+    76: tom
+  }
+  
+  function keyDown(e) {
+    let keyCode = e.keyCode
+    let key = document.querySelector(`.key[data-key="${keyCode}"]`)
+    let audio = sounds[keyCode]
+    if(!audio || !key) return
+    
+    key.classList.add("pressed")
+    audio.currentTime = 0
+    audio.play()
+    if(keyCode == 65) boom.play()
+  }
+  
+  function keyUp(e) {
+    let keyCode = e.keyCode
+    let key = document.querySelector(`.key[data-key="${keyCode}"]`)
+    if(!key) return
+  
+    key.classList.remove("pressed")
+  }
