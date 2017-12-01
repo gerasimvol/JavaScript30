b3916c712d296001252dd3863af10b88fdd45416
ADRABA\vladimir.gerasimenko <vol.gerasimenko@gmail.com>
2017-12-01 16:06:23 +0200

    Try to fix audio delay

--- 01-Drums/script.js
+++ 01-Drums/script.js
@@ -1,7 +1,8 @@
-  window.addEventListener("keydown", keyDown, false)
-  window.addEventListener("keyup", keyUp, false)
+  function loaded() {
+    window.addEventListener("keydown", keyDown, false)
+    window.addEventListener("keyup", keyUp, false)
+  }
 
-  
   let boom = new Audio("sounds/boom.wav"),
       clap = new Audio("sounds/clap.wav"),
       hihat = new Audio("sounds/hihat.wav"),
@@ -11,6 +12,16 @@
       snare = new Audio("sounds/snare.wav"),
       tink = new Audio("sounds/tink.wav"),
       tom = new Audio("sounds/tom.wav")
+
+      boom.addEventListener('canplaythrough', loaded, false)
+      clap.addEventListener('canplaythrough', loaded, false)
+      hihat.addEventListener('canplaythrough', loaded, false)
+      kick.addEventListener('canplaythrough', loaded, false)
+      openhat.addEventListener('canplaythrough', loaded, false)
+      ride.addEventListener('canplaythrough', loaded, false)
+      snare.addEventListener('canplaythrough', loaded, false)
+      tink.addEventListener('canplaythrough', loaded, false)
+      tom.addEventListener('canplaythrough', loaded, false)
   
   const sounds = {
     65: boom,
@@ -23,6 +34,8 @@
     75: tink,
     76: tom
   }
+
+  
   
   function keyDown(e) {
     let keyCode = e.keyCode
