2bbcd09a78ff3a171f42d9b8cfae1de1e302383c
ADRABA\vladimir.gerasimenko <vol.gerasimenko@gmail.com>
2017-12-14 20:38:28 +0200

    Fix bug checkboxes

--- 07-MultipleCheckbox/script.js
+++ 07-MultipleCheckbox/script.js
@@ -4,22 +4,34 @@ checkboxes.forEach((checkbox) => {
   checkbox.addEventListener('click', changeCheckbox)
 })
 
+
 function changeCheckbox(e) {
   if(!e.shiftKey) return
+  let done = false
   
   const cbIndex = checkboxes.indexOf(e.target)
 
-  for(let i = cbIndex; i <= checkboxes.length; i++) {
-    try {
-      if(checkboxes[i].checked) {
-        markCheckboxes(i, cbIndex, checkboxes, true)
-      } 
-    } catch(e) {
-      for(let j = cbIndex; j >= 0; j--) {
-        if(checkboxes[j].checked) {
-          markCheckboxes(j, cbIndex, checkboxes, false)
+  for(let i = cbIndex+1; i <= checkboxes.length; i++) {
+    if(!done) {
+      try {
+        if(checkboxes[i].checked) {
+          markCheckboxes(i, cbIndex, checkboxes, true)
+          done = true
+          return
+        } 
+      } catch(e) {
+        if(!done) {
+          for(let j = cbIndex-1; j >= 0; j--) {
+            if(checkboxes[j].checked) {
+              markCheckboxes(j, cbIndex, checkboxes, false)
+              done = true
+              return
+            }
+          }
         }
       }
+    } else {
+      return
     }
   }
 }
