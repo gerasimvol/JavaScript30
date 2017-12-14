6e7c9823b3902842f0c8ee0f051d6608eab3acb3
ADRABA\vladimir.gerasimenko <vol.gerasimenko@gmail.com>
2017-12-14 20:42:16 +0200

    Add comments to checkboxes

--- 07-MultipleCheckbox/script.js
+++ 07-MultipleCheckbox/script.js
@@ -6,24 +6,34 @@ checkboxes.forEach((checkbox) => {
 
 
 function changeCheckbox(e) {
+  // if not with shift we dont care
   if(!e.shiftKey) return
+  // store if we finished matching between shift pressed and first found
   let done = false
-  
+  // shift pressed checkbox
   const cbIndex = checkboxes.indexOf(e.target)
 
+  // try to find from up to down
   for(let i = cbIndex+1; i <= checkboxes.length; i++) {
     if(!done) {
       try {
+        // found first checked!
         if(checkboxes[i].checked) {
+          // mark all between
           markCheckboxes(i, cbIndex, checkboxes, true)
+          // we done matching
           done = true
           return
         } 
       } catch(e) {
         if(!done) {
+          // try to find from down to up
           for(let j = cbIndex-1; j >= 0; j--) {
+            // found first checked!
             if(checkboxes[j].checked) {
+              // mark all between
               markCheckboxes(j, cbIndex, checkboxes, false)
+              // we done matching
               done = true
               return
             }
